236cb6f074cc1654ca2f9962f0580e020f36f8e4
cmcmone <cmcmone@gmail.com>
2021-04-03 15:10:35 -0400

    footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import '../../assets/css/footer.css'
 
 export default function Footer() {
-  return <div>Here is the Footer</div>;
+  return (
+    <div className="footer">
+      <span>
+        <a href="https://github.com/cmcmone">Developed by Roy Chen</a>
+      </span>
+      <span className="github-repo">
+        <a
+          href="https://github.com/cmcmone/reactjs-movies-app"
+          title="View Github Repo"
+        >
+          View Code
+        </a>
+      </span>
+    </div>
+  );
 }
