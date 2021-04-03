c2fb7f2cfca416d7e2503eb87b25f45e42cd816e
cmcmone <cmcmone@gmail.com>
2021-04-03 15:10:53 -0400

    search component

--- src/components/header/Search/index.jsx
+++ src/components/header/Search/index.jsx
@@ -1,23 +1,70 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
+import axios from 'axios';
 
-import { SearchContext } from '../../../Context';
+import { axiosKeywordConfig, debounce } from '../../common/common';
+import { MovieInfoContext } from '../../../Context';
 
 export default function Search() {
-  const [, setSearchContext] = useContext(SearchContext);
+  const [, setMovieID] = useContext(MovieInfoContext);
+  const [datalist, setDatalist] = useState([]);
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const newList = datalist.find((item) => item.title === inputEl.current);
+    if (!!newList) {
+      setMovieID(() => {
+        return newList.id;
+      });
+    }
+  }, [datalist, setMovieID]);
+
   return (
-    <div>
+    <div className='search'>
       <input
+        list="movies"
         placeholder="Search Movie Title ..."
-        onKeyDown={(e) => {
-          handleKeyDown(e, setSearchContext);
+        ref={inputEl}
+        onChange={(event) => handleChange(event, setDatalist, inputEl)}
+        onBlur={() => {
+          setDatalist(() => {
+            return [];
+          });
         }}
       />
+      <datalist id="movies">
+        {datalist.map((item) => {
+          return <option key={item.id} value={item.title}></option>;
+        })}
+      </datalist>
     </div>
   );
 }
 
-function handleKeyDown(e, setSearchContext) {
-  if (e.keyCode === 13) {
-    setSearchContext(e.target.value);
+const handleChange = debounce((event, setDatalist, inputEl) => {
+  const { value } = event.target;
+  inputEl.current = value;
+  if (value.length > 1) {
+    moviesRequest(value, setDatalist);
   }
-}
+}, 500);
+
+const moviesRequest = (query, setDatalist) => {
+  const { baseURL, api_key, page, include_adult } = axiosKeywordConfig;
+  return axios
+    .get(baseURL, {
+      params: {
+        api_key,
+        query,
+        page,
+        include_adult,
+      },
+    })
+    .then((response) => {
+      setDatalist(() => {
+        return response.data.results;
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
